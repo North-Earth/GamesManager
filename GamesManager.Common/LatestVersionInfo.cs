53745fe192a3518b7689596c6806d42036e5c724
Revolt27 <kuckushckin.alex@yandex.ru>
2019-10-12 11:19:29 +0300

    Added common library

---
+++ GamesManager.Common/LatestVersionInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamesManager.Common
+{
+    public class LatestVersionInfo
+    {
+        public int Id { get; set; }
+
+        public string Version { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
