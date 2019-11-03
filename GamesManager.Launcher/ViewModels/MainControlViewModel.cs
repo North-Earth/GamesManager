b2372178ca63954a858fac42ec37fc4750705288
Revolt27 <kuckushckin.alex@yandex.ru>
2019-11-03 16:02:05 +0300

    Added new elements insterface

---
+++ GamesManager.Launcher/ViewModels/MainControlViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MVVM_Helper.Binding;
+
+namespace GamesManager.Launcher.ViewModels
+{
+    public class MainControlViewModel : ObservableObject
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+
+        public MainControlViewModel()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
---
+++ GamesManager.Launcher/ViewModels/ProductItemViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MVVM_Helper.Binding;
+
+namespace GamesManager.Launcher.ViewModels
+{
+    public class ProductItemViewModel : ObservableObject
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+
+        public ProductItemViewModel()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
