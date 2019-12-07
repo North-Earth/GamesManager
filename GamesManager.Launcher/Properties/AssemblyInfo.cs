f962cf6351f83caed0e58e376362e220fa0fa4b6
Revolt27 <kuckushckin.alex@yandex.ru>
2019-12-07 18:25:44 +0300

    Added icon and updeted Assembly

--- GamesManager.Launcher/Properties/AssemblyInfo.cs
+++ GamesManager.Launcher/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GamesManager.Launcher")]
+[assembly: AssemblyTitle("AK's Games Launcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GamesManager.Launcher")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("Alexey Kukushkin")]
+[assembly: AssemblyProduct("Alexey Kukushkin's Games Launcher")]
+[assembly: AssemblyCopyright("Alexey Kukushkin ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
