48898ab6d447f16f89909c688f63709855ff8240
Revolt27 <kuckushckin.alex@yandex.ru>
2019-12-12 22:48:47 +0300

    Created new project dotnet core 3.1

---
+++ GamesManager.Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamesManager.Client.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+
+        public MainWindowViewModel() { }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
